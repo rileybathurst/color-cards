ed966cc53466078fcf86e7372dc8bb7bb4dae08d
Riley <riley@rileybathurst.com>
2022-04-21 14:59:33 -0700

    im a little lost here

---
+++ src/components/TestButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import styles from './TestButton.module.css';
+
+const TestButton = () => {
+  return (
+    <button>Go</button>
+  );
+}
+
+export default TestButton;
